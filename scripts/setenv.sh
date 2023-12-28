1e9eeb05f781db50adbf282de9419add3eb8ba60
lee <anhldpro@yahoo.com>
2023-12-27 23:22:03 -0500

    fx

---
+++ scripts/setenv.sh
@@ -0,0 +1,15 @@
+
+export GIT_USERNAME=anhldpro
+export GIT_PRIV_TOKEN=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
+
+export EMAIL_HOST=sandbox.smtp.mailtrap.io
+export EMAIL_PASSWORD=92399975b7039f
+export EMAIL_PORT=587
+export EMAIL_USERNAME=1c6f5d11b63cff
+
+export PROV_ORG_OWNER_EMAIL=anhld@seatechit.com.vn
+export PROV_ORG_OWNER_FIRST_NAME=anh
+export PROV_ORG_OWNER_LAST_NAME=le
+export PROV_ORG_OWNER_PASSWORD=admin@123
+export PROV_ORG_OWNER_USERNAME=admin
+export PROV_ORG_TITLE=bu1
