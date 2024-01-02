08a6f0a0b7c455b4f3ffee21fd4a1854878c5717
lee <anhldpro@yahoo.com>
2024-01-02 07:23:39 -0500

    fx

--- scripts/config_apicv10.py
+++ scripts/config_apicv10.py
@@ -503,7 +503,7 @@ try:
 	admin_bearer_token = api_calls.get_bearer_token(environment_config["APIC_API_MANAGER_URL"],
 													os.environ["PROV_ORG_OWNER_USERNAME"],
 													os.environ["PROV_ORG_OWNER_PASSWORD"],
-													"provider/default-idp-2",
+													"provider/default-idp-1",
 													toolkit_credentials["toolkit"]["client_id"],
 													toolkit_credentials["toolkit"]["client_secret"])
 	if DEBUG:
